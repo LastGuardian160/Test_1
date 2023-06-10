b350ac9a373206fea7e2d8360fc5a27f3d347333
LastGuardian160 <135696829+LastGuardian160@users.noreply.github.com>
2023-06-10 13:29:05 +0300

    Test_test.py

--- Test_test.py
+++ Test_test.py
@@ -1,7 +1,20 @@
 from Test import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+ui = Ui_MainWindow()
 
 
-ui = Ui_MainWindow() # создание объекта класса Ui_MainWindow
-value = ui.sliderValueChanged() # вызов метода sliderValueChanged(), чтобы получить значение value
-print(value)
+def value():
+    value = ui.sliderValueChanged()  # вызов метода sliderValueChanged(), чтобы получить значение value
+    print(value)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    value()
+    sys.exit(app.exec())
 
